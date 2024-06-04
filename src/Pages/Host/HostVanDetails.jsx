584ab2e0e992add1349d476397824f9519d0b442
surendermeena <106897366+surendermeena@users.noreply.github.com>
2024-06-04 11:15:32 +0000

    VAN host details page CSS added

--- src/Pages/Host/HostVanDetails.jsx
+++ src/Pages/Host/HostVanDetails.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 const HostVanDetails = () => {
-  return (
-    <div>
-      This is Host van Details
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = React.useState(null);
 
-export default HostVanDetails
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <section>
+      <Link to=".." relative="path" className="back-button">
+        &larr; <span>Back to all vans</span>
+      </Link>
+
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} width={150} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HostVanDetails;
